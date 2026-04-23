3937197fe3ef918f06342880eaafdc8f64b02add
Devara Wiraka <devaraalfaritzi26@gmail.com>
2026-04-23 12:54:26 +0700

    fix: nextresponse.next payload formatting in proxy

--- proxy.ts
+++ proxy.ts
@@ -19,7 +19,9 @@ export async function proxy(request: NextRequest) {
         setAll(cookiesToSet) {
           cookiesToSet.forEach(({ name, value }) => request.cookies.set(name, value))
           supabaseResponse = NextResponse.next({
-            request,
+            request: {
+              headers: request.headers,
+            },
           })
           cookiesToSet.forEach(({ name, value, options }) =>
             supabaseResponse.cookies.set(name, value, options)
